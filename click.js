87100a98af767d13f9b8f549cfb4af8b79100a21
snrshohan <snrshohan21@gmail.com>
2023-02-14 08:54:35 -0900

    done

---
+++ click.js
@@ -0,0 +1,12 @@
+function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+   }
+   function makeRed(){
+    document.body.style.backgroundColor = 'red';
+   }
+   function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+   }
+   function makeYellow(){
+    document.body.style.backgroundColor = 'yellow';
+   }
